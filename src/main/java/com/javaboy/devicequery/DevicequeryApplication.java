d024c5b5f83158dfcfdfde693bf7016f49e60b9e
shenxt <761764136@qq.com>
2020-07-10 14:39:20 +0800

    定时任务

--- src/main/java/com/javaboy/devicequery/DevicequeryApplication.java
+++ src/main/java/com/javaboy/devicequery/DevicequeryApplication.java
@@ -3,9 +3,11 @@ package com.javaboy.devicequery;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan(basePackages ="com.javaboy.devicequery.mapper")
+@EnableScheduling
 public class DevicequeryApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/javaboy/devicequery/task/testSchedul.java
@@ -0,0 +1,21 @@
+package com.javaboy.devicequery.task;
+
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author shenxt
+ */
+@Component
+public class testSchedul {
+
+    int i=1;
+    @Scheduled(cron = "${cron.timer}")
+    public void test(){
+        System.out.println(i++);
+    }
+
+}
